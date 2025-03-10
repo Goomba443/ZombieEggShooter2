9e76ede24f8d8567bca0e8f456edaf04a101cc23
Inés <isanchezq@campusdigitalfp.com>
2025-03-10 19:00:43 +0100

    Muerte arreglada

--- Assets/Scripts/playerUIMAnager.cs
+++ Assets/Scripts/playerUIMAnager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playerUIMAnager : MonoBehaviour
 {
+    public GameObject deathScreen;
+    public GameObject gameplayUI;
     private TextMeshProUGUI txtHealth;
     private TextMeshProUGUI txtEggs;
     private Weapon weapon;
+    public Button playAgainBtn;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +22,7 @@ public class playerUIMAnager : MonoBehaviour
         txtEggs = GameObject.Find("EggsLeft").GetComponent<TextMeshProUGUI>();
         txtHealth.text = "Vida: " + PlayerHealth.maxHealth.ToString();
         txtEggs.text = "Huevos: " + Weapon.cargador.ToString();
-
+        playAgainBtn.onClick.AddListener(StartOver);
     }
 
     // Update is called once per frame
@@ -27,7 +33,17 @@ public class playerUIMAnager : MonoBehaviour
 
     public void UpdateText()
     {
-        txtHealth.text = "Vida: " + PlayerHealth.playerHealth.ToString();
+        if (PlayerHealth.playerHealth > 0)
+        {
+            txtHealth.text = "Vida: " + PlayerHealth.playerHealth.ToString();
+        } else
+        {
+            //txtHealth.text = "ES INFINITA PQ AHORA ERES UN ZOMBIE";
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            gameplayUI.gameObject.SetActive(false);
+            deathScreen.gameObject.SetActive(true);
+        }
         if(Weapon.cargador > 0)
         {
             txtEggs.text = "Huevos: " + Weapon.cargador.ToString();
@@ -35,5 +51,10 @@ public class playerUIMAnager : MonoBehaviour
         {
             txtEggs.text = "Pulsa 'R' para recargar";
         }
+    }
+
+    public void StartOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
